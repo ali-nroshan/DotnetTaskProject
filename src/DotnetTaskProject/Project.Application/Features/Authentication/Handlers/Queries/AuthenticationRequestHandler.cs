78fb1f2ae49832960e6ee5fe184a95caa30a6aa1
ali <ali.nr.job@gmail.com>
2023-07-07 09:50:33 -0400

    implement authentication feature in application layer

---
+++ src/DotnetTaskProject/Project.Application/Features/Authentication/Handlers/Queries/AuthenticationRequestHandler.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Project.Application.Contracts.Infrastructure;
+using Project.Application.Contracts.Persistence;
+using Project.Application.Exceptions;
+using Project.Application.Features.Authentication.Requests.Queries;
+
+namespace Project.Application.Features.Authentication.Handlers.Queries
+{
+    public class AuthenticationRequestHandler : IRequestHandler<
+		AuthenticationRequest, string>
+	{
+		private readonly IUserRepository _userRepository;
+		private readonly IJwtTokenGenerator _jwtTokenGenerator;
+		private readonly IPasswordHasher _passwordHasher;
+
+        public AuthenticationRequestHandler(IUserRepository userRepository, IJwtTokenGenerator jwtTokenGenerator, IPasswordHasher passwordHasher)
+        {
+            _userRepository = userRepository;
+			_jwtTokenGenerator = jwtTokenGenerator;
+			_passwordHasher = passwordHasher;
+        }
+
+        public async Task<string> Handle(AuthenticationRequest request, CancellationToken cancellationToken)
+		{
+			string passwordHash = _passwordHasher.HashPassword(request.AuthenticationRequestModel.Password);
+			var userId = await _userRepository.CheckUserCredentialsAsync(request.AuthenticationRequestModel.UserName, passwordHash);
+			if (userId == null)
+				throw new UnauthorizedException();
+
+			string token = _jwtTokenGenerator.GenerateToken(userId.Value);
+			return token;
+        }
+	}
+}
---
+++ src/DotnetTaskProject/Project.Application/Features/Authentication/Requests/Queries/AuthenticationRequest.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Project.Application.Models.Identity;
+
+namespace Project.Application.Features.Authentication.Requests.Queries
+{
+    public class AuthenticationRequest : IRequest<string>
+	{
+        public AuthenticationRequestModel AuthenticationRequestModel { get; set; }
+    }
+}
