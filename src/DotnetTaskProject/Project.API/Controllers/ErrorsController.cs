855a30e3dd143f1610b915a89837b2a1c535037a
ali <ali.nr.job@gmail.com>
2023-07-07 10:00:23 -0400

    implement ErrorsController

---
+++ src/DotnetTaskProject/Project.API/Controllers/ErrorsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Project.Application.Exceptions;
+
+namespace Project.API.Controllers
+{
+    [ApiExplorerSettings(IgnoreApi = true)]
+	[ApiController]
+	public class ErrorsController : ControllerBase
+	{
+		[Route("/Error")]
+		public ActionResult ErrorHandler()
+		{
+			Exception? exception = HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
+
+			switch (exception)
+			{
+				case BadRequestException: return BadRequest();
+				case NotFoundException: return NotFound();
+				case UnauthorizedException: return Unauthorized();
+				default: return StatusCode(500);
+			}
+		}
+	}
+}
