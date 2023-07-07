a6cac5ee9d3d577a5f16c59e417d7a01ff9fe6b8
ali <ali.nr.job@gmail.com>
2023-07-07 09:47:44 -0400

    implement jwt_token_generator and jwt_settings and passowrd_hasher

---
+++ src/DotnetTaskProject/Project.Infrastructure/Authentication/JwtTokenGenerator.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Project.Application.Contracts.Infrastructure;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Project.Infrastructure.Authentication
+{
+	public class JwtTokenGenerator : IJwtTokenGenerator
+	{
+		private readonly JwtSettings _jwtSettings;
+
+        public JwtTokenGenerator(IOptions<JwtSettings> jwtSettings)
+        {
+            _jwtSettings = jwtSettings.Value;
+        }
+
+        public string GenerateToken(Guid userId)
+		{
+			var signingCredentials = new SigningCredentials(
+				new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.Secret)),
+				SecurityAlgorithms.HmacSha256);
+
+			var claims = new[]
+			{
+				new Claim("userid", userId.ToString())
+			};
+
+			var token = new JwtSecurityToken(
+				issuer: _jwtSettings.Issuer,
+				audience: _jwtSettings.Audience,
+				claims: claims,
+				expires: DateTime.UtcNow.AddMinutes(_jwtSettings.DurationInMinutes),
+				signingCredentials: signingCredentials);
+
+			return new JwtSecurityTokenHandler().WriteToken(token);
+		}
+	}
+}
---
+++ src/DotnetTaskProject/Project.Infrastructure/Authentication/PasswordHasher.cs
@@ -0,0 +1,18 @@
+﻿using Project.Application.Contracts.Infrastructure;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Project.Infrastructure.Authentication
+{
+	public class PasswordHasher : IPasswordHasher
+	{
+		public string HashPassword(string password)
+		{
+			using (SHA512 sha512 = SHA512.Create())
+			{
+				var hashBytes = sha512.ComputeHash(Encoding.UTF8.GetBytes(password));
+				return Convert.ToBase64String(hashBytes);
+			}
+		}
+	}
+}
