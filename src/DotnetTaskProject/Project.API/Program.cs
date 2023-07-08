c49ece7b0666136b43d3c653a6181877c2b45c9a
ali <ali.nr.job@gmail.com>
2023-07-08 11:17:02 -0400

    wire up infrastructure dependency and reconfig program.cs

--- src/DotnetTaskProject/Project.API/Program.cs
+++ src/DotnetTaskProject/Project.API/Program.cs
@@ -1,4 +1,5 @@
 using Project.Application;
+using Project.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.RegisterApplicationDependency();
+builder.Services.RegisterInfrastructureDependency(builder.Configuration).RegisterApplicationDependency();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
---
+++ src/DotnetTaskProject/Project.Infrastructure/DependencyContainer.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Project.Application.Contracts.Infrastructure;
+using Project.Application.Contracts.Persistence;
+using Project.Infrastructure.Authentication;
+using Project.Infrastructure.Persistence.Context;
+using Project.Infrastructure.Persistence.Repositories;
+using System.Text;
+
+namespace Project.Infrastructure
+{
+	public static class DependencyContainer
+	{
+		public static IServiceCollection RegisterInfrastructureDependency(this IServiceCollection services, IConfiguration configuration)
+		{
+			#region Persistence
+			services.AddDbContext<ProjectDbContext>(options =>
+			{
+				options.UseSqlServer(configuration.GetConnectionString("ProjectDbContext"));
+			});
+
+			services.AddScoped<IProductRepository, ProductRepository>();
+			services.AddScoped<IUserRepository, UserRepository>();
+			#endregion
+			#region Infrastructure
+			services.AddSingleton<IPasswordHasher, PasswordHasher>();
+
+			services.Configure<JwtSettings>(configuration.GetSection(nameof(JwtSettings)));
+			services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+
+			var jwtSettings = new JwtSettings();
+			configuration.Bind(nameof(JwtSettings), jwtSettings);
+
+			services.AddAuthentication(defaultScheme: JwtBearerDefaults.AuthenticationScheme)
+				.AddJwtBearer(options =>
+				{
+					options.TokenValidationParameters = new TokenValidationParameters()
+					{
+						ValidateIssuer = true,
+						ValidateAudience = true,
+						ValidateLifetime = true,
+						ValidateIssuerSigningKey = true,
+						ValidAlgorithms = new[] { SecurityAlgorithms.HmacSha256 },
+						ValidIssuer = jwtSettings.Issuer,
+						ValidAudience = jwtSettings.Audience,
+						IssuerSigningKey = new SymmetricSecurityKey(
+							Encoding.UTF8.GetBytes(jwtSettings.Secret))
+					};
+				});
+			#endregion
+			return services;
+		}
+	}
+}
